b096956b5d194a3249848da12ca13c689e404fb6
Gilhyun RYOU <gryou@bangka.ad.europe.naverlabs.com>
2018-07-12 14:59:14 +0200

    temp

--- basics/1_interface.py
+++ basics/1_interface.py
@@ -3,3 +3,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import gym
+from IPython import display
+import matplotlib.pyplot as plt
+
+env = gym.make('Breakout-v0')
+env.reset()
+for _ in range(1000):
+    plt.imshow(env.render(mode='rgb_array'))
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    env.step(env.action_space.sample())
